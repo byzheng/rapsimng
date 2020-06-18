8fe9de54ea519506230df35f3df32d9f5d721abd
Bangyou Zheng <bangyou.zheng@csiro.au>
2020-06-18 15:28:24 +1000

    Add vignettes

--- R/apsimng.R
+++ R/apsimng.R
@@ -40,7 +40,7 @@ write_apsimx <- function(l, file) {
 }
 
 
-#' Find an element in the apsimx file
+#' Find an element in the apsimx file (first occurrence)
 #'
 #' @param l the list of apsimx file
 #' @param max_depth The maximum depth to search
@@ -56,7 +56,7 @@ write_apsimx <- function(l, file) {
 #' potential <- search_node(m,
 #'             Name = 'PotentialBranchingRate',
 #'             XProperty = "[Structure].LeafTipsAppeared")
-search_node <- function(l, max_depth = 100000, ...) {
+search_node <- function(l, max_depth = 1000000, ...) {
     conds <- list(...)
     ele_names <- names(conds)
 
@@ -234,6 +234,38 @@ insert_model <- function(l, path, model) {
     eval(parse(text=eq_str))
     return(l)
 }
+
+
+#' append a model into apsimx
+#'
+#' @param l the list of apsimx file
+#' @param path If numeric, the path returned by search_path or search_node. If character, the path supported by apsimx
+#' @param model A new model
+#' @return The modified list with new value
+#' @export
+#'
+append_model <- function(l, path, model) {
+    path <- .check_path(l, path)
+    if (length(path) == 1) {
+        stop("Path should be more than 1")
+    }
+    if (length(model) > 1) {
+        stop("The length of new model should be equal to 1")
+    }
+    eq <- 'l'
+    path1 <- path[-length(path)]
+    path2 <- path[length(path)]
+    for (i in seq(along = path1)) {
+        eq <- c(eq, '[["Children"]]', paste0('[[', path[i], ']]'))
+    }
+    eq <- c(eq, '[["Children"]]')
+    eq_str <- paste(eq, collapse = '')
+    eq_str <- paste0(eq_str, ' <- append(', eq_str, ', model, ', path2, ')')
+
+    eval(parse(text=eq_str))
+    return(l)
+}
+
 #' Convert a model into xypair
 #'
 #' @param l the list of apsimx file
