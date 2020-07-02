cd0fb4333f4e3f26e38c9635d9e3148f0ce18354
Bangyou Zheng <bangyou.zheng@csiro.au>
2020-07-02 19:02:46 +1000

    Update vignettes

--- doc/vignettes.R
+++ doc/vignettes.R
@@ -3,8 +3,9 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
+
 # install packages
-if (!require(RApsimNG1)) {
+if (!require(RApsimNG)) {
     devtools::install_github('byzheng/RApsimNG')
 }
 library(RApsimNG)
@@ -12,6 +13,7 @@ library(RApsimNG)
 ## ----read-apsimx--------------------------------------------------------------
 # Read Wheat.apsimx file with `read_apsimx` which returns a list of json results.
 wheat_apsimx <- read_apsimx("https://raw.githubusercontent.com/APSIMInitiative/ApsimX/master/Tests/Validation/Wheat/Wheat.apsimx")
+summary(wheat_apsimx)
 
 
 ## ----search-path--------------------------------------------------------------
