424cc68c7f524985a6c456e6baf21feacd99bf52
Bangyou Zheng <bangyou.zheng@csiro.au>
2020-09-29 09:59:05 +1000

    Update the path

--- tests/testthat/test-search.R
+++ tests/testthat/test-search.R
@@ -1,7 +1,7 @@
 suppressPackageStartupMessages(library(testthat))
 test_that("Search all nodes", {
     # Read wheat.json
-    wheat <- read_apsimx(system.file("wheat.json", package = "RApsimNG"))
+    wheat <- read_apsimx(read_apsimx("inst/Wheat.json"))
     # Search first node
     # Empty if no matching
     a <- search_node(wheat, Name = "Simulations1")
