78170007beccf5518bce3322d2f5e28b27922e49
Bangyou Zheng @ RStudio Server <bangyou.zheng@csiro.au>
2024-02-01 12:06:25 +1100

    Remove report test

--- tests/testthat/test-report.R
+++
@@ -1,9 +0,0 @@
-suppressPackageStartupMessages(library(testthat))
-test_that("report", {
-    file <- system.file("extdata/wheat.apsimx", package = "rapsimng")
-    res <- read_report(file, "HarvestReport")
-    expect_error(read_report(file, "Harvest"))
-    expect_equal(nrow(res), 12)
-    rpts <- list_report(file)
-    expect_equal(rpts, "HarvestReport")
-})
