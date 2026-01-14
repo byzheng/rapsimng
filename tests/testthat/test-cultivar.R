d9c0e53967c65950f92ebc6958df1af68ebffb7d
Bangyou Zheng <zheng.bangyou@gmail.com>
2026-01-14 14:17:24 +1000

    Add test

--- tests/testthat/test-cultivar.R
+++ tests/testthat/test-cultivar.R
@@ -3,9 +3,12 @@ test_that("cultivar", {
     wheat <- read_apsimx(system.file("extdata/Wheat.json", package = "rapsimng"))
     a <- get_cultivar(wheat)
     expect_equal(nrow(a), 30)
+    expect_equal(length(a$name), 30)
+    expect_equal(length(a$standard_name), 30)
+    expect_equal(length(a$parameter), 30)
+    expect_equal(length(a$value), 30)
     a <- get_cultivar(wheat, alias = FALSE)
     expect_equal(nrow(a), 25)
-
 })
 
 
