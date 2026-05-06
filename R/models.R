77f00c32b93b6500e318c09c99c30c10a5cb16e3
Bangyou Zheng (laptop) <zheng.bangyou@gmail.com>
2026-05-06 20:16:46 +1000

    Bump version to 0.5.0, remove available_models function and its documentation, and update new_model function to use .get_models

--- R/models.R
+++ R/models.R
@@ -1,43 +1,3 @@
-# * Author:    Bangyou Zheng (Bangyou.Zheng@csiro.au)
-# * Created:   02:53 PM Friday, 19 June 2020
-# * Copyright: AS IS
-
-
-# Models of APSIMNG
-pkg_env <- new.env()
-.read_assembly <- function() {
-
-  if (!exists(".rapsimng_models", envir = pkg_env)) {
-
-    file <- system.file("extdata/Models.Rds", package = "rapsimng")
-    if (!file.exists(file)) {
-      file <- "extdata/Models.Rds"
-      if (!file.exists(file)) {
-        stop("Models.xml doesn't exist.")
-      }
-    }
-    .rapsimng_models <- readRDS(file)
-    assign(".rapsimng_models", .rapsimng_models, envir = pkg_env)
-  } else {
-    .rapsimng_models <- get(".rapsimng_models", envir = pkg_env)
-  }
-  .rapsimng_models
-}
-
-#' List all available models in APSIM NG
-#'
-#' @return a character vector of available models
-#' @export
-#'
-#' @examples
-#' a <- available_models()
-#' a[1:10]
-available_models <- function() {
-  models <- .read_assembly()
-  # Find model
-  names(models)
-}
-
 
 #' Create a new model
 #'
@@ -49,22 +9,51 @@ available_models <- function() {
 #' new_model(model = "PMF.Cultivar", name = "example")
 #' @export
 new_model <- function(model, name = model) {
-  if (length(model) != 1 || length(name) != 1) {
-    stop("Only support 1 character value model")
-  }
-  # remove name space
-  if (name == model) {
-    name <- gsub("^([a-zA-Z_]+)(\\.*)(.*)$", "\\3", model)
-    if (nchar(name) == 0) {
-      name <- model
+    if (length(model) != 1 || length(name) != 1) {
+        stop("Only support 1 character value model")
     }
-  }
-  models <- .read_assembly()
-  # Find model
-  m <- models[[model]]
-  if (is.null(m)) {
-    stop("Cannot find the models with name ", model)
-  }
-  m$Name <- name
-  m
+    # remove name space
+    if (name == model) {
+        name <- gsub("^([a-zA-Z_]+)(\\.*)(.*)$", "\\3", model)
+        if (nchar(name) == 0) {
+        name <- model
+        }
+    }
+    models <- .get_models()
+    # Find model
+    m <- models[[model]]
+    if (is.null(m)) {
+        stop("Cannot find the models with name ", model)
+    }
+    m$Name <- name
+    m
+}
+
+
+.get_models <- function() {
+    define <- list(
+        "PMF.Cultivar" = list(
+            "$type" = "Models.PMF.Cultivar, Models",
+            "Command" =  list(),
+            "Name" = "Cultivar",
+            "ResourceName" = NULL,
+            "Children" = list(),
+            "Enabled" = TRUE,
+            "ReadOnly" = TRUE
+        ), 
+        "Core.Folder" = list(
+            "$type" = "Models.Core.Folder, Models",
+            "ShowInDocs" = FALSE,
+            "Name" = "Folder",
+            "ResourceName" = NULL,
+            "Children" = list(),
+            "Enabled" = TRUE,
+            "ReadOnly" = FALSE
+        )
+    )
+    define[["Core.Replacements"]] <- define[["Core.Folder"]]
+    define[["Core.Replacements"]]$Name <- "Replacements"
+    define[["PMF.CultivarFolder"]] <- define[["Core.Folder"]]
+    define[["PMF.CultivarFolder"]]$Name <- "CultivarFolder"
+    define
 }
